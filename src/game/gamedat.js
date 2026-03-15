f79ae4542748135bb36faade80796d334ecb9b94
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-15 15:10:58 -0400

    Adjust comments.

--- src/game/gamedat.js
+++ src/game/gamedat.js
@@ -36,13 +36,13 @@ window.gamedat_ids = {};
 
 (function() {
     gamedat_ids.GAMEID = 'zork1-r88-s840726';
-    gamedat_ids.MAX_OBJECTS = 250;
-    gamedat_ids.MAX_GLOBALS = 158;
-    gamedat_ids.DICT_START = 15137;         // header word $08
+    gamedat_ids.MAX_OBJECTS = 250;         // "Object count"
+    gamedat_ids.MAX_GLOBALS = 158;         // 1+LastGlobal
+    gamedat_ids.DICT_START = 15137;        // header word $08
     gamedat_ids.MAX_DICT_WORD = window.gamedat_dictwords.length;
     gamedat_ids.DICT_WORD_SIZE = 7;
-    gamedat_ids.PROP_TABLE_START = 0x0BB8;  // prop address for first obj
-    gamedat_ids.PROP_TABLE_END = 0x2270;    // just before globals, header $0C
+    gamedat_ids.PROP_TABLE_START = 0x0BB8; // prop address for first obj
+    gamedat_ids.PROP_TABLE_END = 0x2270;   // just before globals, header $0C-1
     gamedat_ids.C_TABLE_LEN = 180;
 
     for (let obj of window.gamedat_properties) {
